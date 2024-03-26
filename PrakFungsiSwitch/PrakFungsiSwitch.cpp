ac8b69fabffd702fc0baa6875802abab356ff7fe
tacosz <nabilnasruddin.offc@gmail.com>
2024-03-26 17:36:29 +0700

    void input

--- PrakFungsiSwitch/PrakFungsiSwitch.cpp
+++ PrakFungsiSwitch/PrakFungsiSwitch.cpp
@@ -3,6 +3,14 @@ using namespace std;
 
 int bilA, bilB;
 
+void input()
+{
+    cout << "Masukkan bilangan pertama = ";
+    cin >> bilA;
+    cout << "Masukkan bilangan kedua = ";
+    cin >> bilB;
+}
+
 int main()
 {
     
